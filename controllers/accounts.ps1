27f5d8cf54ef7b108605adfa20fd996d32f7f164
Robert Weber <robert.weber.ctr@navy.mil>
2017-01-26 17:23:54 -0500

    Cleaned up functions that were moved

--- controllers/accounts.ps1
+++ controllers/accounts.ps1
@@ -17,27 +17,7 @@ begin{
 		}
 		
 		
-		[Object[]] getLocalAccounts($computerName){
-			$dormantUsers = @()
-			$localUsers = ([ADSI]"WinNT://$computerName").Children | ? {$_.SchemaClassName -eq 'user'} | ? { $_.properties.lastlogin -lt ( ( ([System.DateTime]::Now).ToUniversalTime() ).AddDays(-1 * $private.Age) ) } | select `
-				@{e={$_.name};n='DisplayName'},`
-				@{e={$_.name};n='Username'},`
-				@{e={$_.properties.lastlogin};n='LastLogon'},`
-				@{e={if($_.properties.userFlags.ToString() -band 2){$true}else{$false} };n='Disabled'},`
-				@{e={$_.path};n='Path'}, `
-				@{e={'Local'};n='AccountType'}
-				
-			foreach($localUser in $localUsers){				
-				$u = new-object -TypeName PSObject | Select AccountType, DisplayName, Username, LastLogon, Disabled, Path
-				foreach($key in (($localUser | gm  -memberType 'NoteProperty' | select -expand Name ) ) ){
-					 $u.$($key) = $localUser.$($key)
-				}
-							
-				$dormantUsers += $u 
-			}
-			
-			return $dormantUsers
-		}
+		
 	
 		[void] showManageLocalAdmins(){
 			
